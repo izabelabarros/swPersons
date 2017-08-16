c20e5797b3ff9785dbb661eceb012c55b48769db
71501100 <71501100@OMEGAB27>
2017-08-16 20:04:56 -0300

    Adicionar arquivos de projeto.

---
+++ WebTestProject/Controllers/PersonsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebTestProject.Models;
+
+namespace WebTestProject.Controllers
+{
+    [RoutePrefix("v1/persons")]
+    public class PersonsController : ApiController
+    {
+        private string Name { get; set; }
+        private static IList<Person> _persons = new List<Person>();
+
+        [Route("{id}/name")]  
+        [HttpGet]
+        public string GetName(int id)
+        {
+            Name = "Izabela";
+            return Name;
+        }
+
+        [Route("{id}/old")]
+        [HttpGet]
+        public int GetIOld(int id)
+        {
+            return id;
+        }
+
+        [Route("{id}/surname")]
+        [HttpGet]
+        public string GetSurName(int id)
+        {
+            return "Barros";
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        public Person GetPersons(int id)
+        {
+            return _persons[id];
+        }
+
+        [Route("{persons}")]
+        [HttpGet]
+        public IList<Person> GetPersons()
+        {
+            return _persons;
+        }
+
+        [Route]
+        [HttpPost]
+        public object CreatePersons(object person)
+        {
+            return person;
+        }
+
+        [Route]
+        [HttpPost]
+        public Person CreatePersons(Person person)
+        {
+            _persons.Add(person);
+            //person.ID = _person.Count;
+            return person;
+        }
+    }
+}
---
+++ WebTestProject/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebTestProject.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int old { get; set; }
+
+    }
+}
